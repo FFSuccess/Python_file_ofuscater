f9468f1fadf7da7e3ee4747e87b6061e05aefcdd
FFSuccess <sdawkinswood@gmail.com>
2025-06-24 10:42:08 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,14 +6,12 @@ file_to_obfuscate = input("Give file path: ")
 passes = int(input("How many times do you want your file to be encrypted? "))
 
 
-def generate_caesar_function():
-    return '''def decrypt(key, message):
-    return_message = ""
-    for char in message:
-        ascii_val = ord(char)
-        shifted = (ascii_val - key) % 256
-        return_message += chr(shifted)
-    return return_message'''
+letters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
+           'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e',
+           'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 
 def encrypt_caesar(key, message):
     return_message = ""
@@ -29,12 +27,35 @@ def encrypt_2(message):
     encrypted = f.encrypt(message.encode('utf-8'))
     return f"\"{key.decode('utf-8')}\"", encrypted.decode('utf-8')
 
-def generate_decrypt_2():
-    return '''def decrypt(key, message):
-    f = Fernet(key.encode('utf-8'))
-    return f.decrypt(message.encode('utf-8')).decode('utf-8')'''
 
 for i in range(1, passes+1):
+    temp = 'Q'
+    for x in range(0, 20):
+        temp += letters[random.randint(0, 61)]
+    temp_2 = 'Q'
+    for x in range(0, 20):
+        temp_2 += letters[random.randint(0, 61)]
+    temp_3 = 'Q'
+    for x in range(0, 20):
+        temp_3 += letters[random.randint(0, 61)]
+    temp_4 = 'Q'
+    for x in range(0, 20):
+        temp_4 += letters[random.randint(0, 61)]
+    def generate_decrypt_2():
+        return f'''def {temp}({temp_2}, {temp_3}):
+        {temp_4} = Fernet({temp_2}.encode('utf-8'))
+        return {temp_4}.decrypt({temp_3}.encode('utf-8')).decode('utf-8')'''
+
+
+    def generate_caesar_function():
+        return f'''def {temp}({temp_2}, {temp_3}):
+        {temp_4} = ""
+        for char in {temp_3}:
+            ascii_val = ord(char)
+            shifted = (ascii_val - {temp_2}) % 256
+            {temp_4} += chr(shifted)
+        return {temp_4}'''
+
     try:
         with open(file_to_obfuscate, "r", encoding='utf-8') as file:
             content = file.read()
@@ -63,7 +84,7 @@ key = {rand_key}
 {func_to_use}
 
 decoded = base64.b64decode(encrypted_data).decode('latin-1')
-original_code = decrypt(key, decoded)
+original_code = {temp}(key, decoded)
 exec(original_code)'''
     with open(file_to_obfuscate, "w", encoding='utf-8') as new_file:
         new_file.write(obfuscated_code)
