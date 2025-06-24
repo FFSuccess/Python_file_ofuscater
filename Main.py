fd50dfa6a1febf85cb23f95284d97c7f65cfb289
FFSuccess <sdawkinswood@gmail.com>
2025-06-24 11:58:00 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import random
 import base64
 from cryptography.fernet import Fernet
+from copy import deepcopy
 
 file_to_obfuscate = input("Give file path: ")
 passes = int(input("How many times do you want your file to be encrypted? "))
@@ -56,22 +57,39 @@ for i in range(1, passes+1):
     temp_9 = 'Q'
     for x in range(0, 20):
         temp_9 += letters[random.randint(0, 61)]
-    temp_10 = 'Q'
-    for x in range(0, 20):
-        temp_10 += letters[random.randint(0, 61)]
+    temp_list = []
+    for x in range(0, 100):
+        temp_10 = 'Q'
+        for y in range(0, 20):
+            temp_10 += letters[random.randint(0, 61)]
+        temp_list.append(temp_10)
+    list_of_vars = deepcopy(temp_list)
+    temp_list.append(temp_9)
+    random.shuffle(temp_list)
+    temp_11 = ''
+
+
     def generate_decrypt_2():
         return f'''def {temp}({temp_2}, {temp_3}):
+    try:
         {temp_4} = Fernet({temp_2}.encode('utf-8'))
-        return {temp_4}.decrypt({temp_3}.encode('utf-8')).decode('utf-8')'''
+        return {temp_4}.decrypt({temp_3}.encode('utf-8')).decode('utf-8')
+    except:
+        pass
+'''
 
 
     def generate_caesar_function():
         return f'''def {temp}({temp_2}, {temp_3}):
+    try:
         {temp_4} = ""
         for {temp_5} in {temp_3}:
             {temp_6} = ord({temp_5})
             {temp_4} += chr(({temp_6} - {temp_2}) % 256)
-        return {temp_4}'''
+        return {temp_4}
+    except:
+        pass
+'''
 
     try:
         with open(file_to_obfuscate, "r", encoding='utf-8') as file:
@@ -86,22 +104,41 @@ for i in range(1, passes+1):
         method = "ceaser"
         rand_key = random.randint(1, 255)
         encrypted_content = encrypt_caesar(rand_key, content)
-        base64_content = base64.b64encode(encrypted_content.encode('latin-1')).decode('ascii')
+        base64_content = base64.b64encode(encrypted_content.encode("latin-1")).decode("ascii")
         func_to_use = generate_caesar_function()
     else:
         method = "encryption"
         rand_key, encrypted_content = encrypt_2(content)
-        base64_content = base64.b64encode(encrypted_content.encode('latin-1')).decode('ascii')
+        base64_content = base64.b64encode(encrypted_content.encode("latin-1")).decode("ascii")
         func_to_use = generate_decrypt_2()
+
+    for x in temp_list:
+        if x != temp_9:
+            if random.randint(1,2) == 1:
+                temp_12 = 'Q'
+                for y in range(0, 43):
+                    temp_12 += letters[random.randint(0, 61)]
+                temp_11 += f'{x} = {temp}("{temp_12}", {temp_8})\n'
+            else:
+                temp_12 = random.randint(1, 255)
+                temp_11 += f'{x} = {temp}({temp_12}, {temp_8})\n'
+        else:
+            temp_11 += f'{temp_9} = {temp}({rand_key}, {temp_8})\n'
     obfuscated_code = f'''import base64
 from cryptography.fernet import Fernet
 {temp_7} = "{base64_content}"
 
 {func_to_use}
 
-{temp_8} = base64.b64decode({temp_7}).decode('latin-1')
-{temp_9} = {temp}({rand_key}, {temp_8})
-exec({temp_9})'''
+{temp_8} = base64.b64decode({temp_7}).decode("latin-1")
+{temp_11}
+rand_list = {str(temp_list).replace("'", "")}
+for i in rand_list:
+    try:
+        exec(i)
+    except:
+        pass
+'''
     with open(file_to_obfuscate, "w", encoding='utf-8') as new_file:
         new_file.write(obfuscated_code)
     print(f"File obfuscated to: {file_to_obfuscate} on pass {i} using {method}.")
