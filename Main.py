a3cc09a76b62d13cb519a88bd7e4ce9d2027ecbe
FFSuccess <sdawkinswood@gmail.com>
2025-06-24 20:41:18 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,8 +13,8 @@ while Number_wanted:
 MAX_FILE_CONTENTS = 1169782
 MAX_NUM_OF_FAKE_KEYS = 50
 MIN_NUM_OF_FAKE_KEYS = 40
-MIN_VARIABLE_LENGTH = 600
-MAX_VARIABLE_LENGTH = 1000
+MIN_VARIABLE_LENGTH = 20
+MAX_VARIABLE_LENGTH = 30
 
 letters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
            'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
