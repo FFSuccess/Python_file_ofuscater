cd6666426f7d539b1f7d54ff0f6866871237c459
FFSuccess <sdawkinswood@gmail.com>
2025-06-24 13:49:30 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 import random
 import base64
 from cryptography.fernet import Fernet
-from copy import deepcopy
 
 file_to_obfuscate = input("Give file path: ")
 passes = int(input("How many times do you want your file to be encrypted? "))
@@ -28,78 +27,73 @@ def encrypt_2(message):
     encrypted = f.encrypt(message.encode('utf-8'))
     return f"\"{key.decode('utf-8')}\"", encrypted.decode('utf-8')
 
+def generate_random_string():
+    string_to_return = 'Q'
+    for x in range(0, random.randint(20,30)):
+        string_to_return += letters[random.randint(0, 61)]
+    return string_to_return
 
+def generate_random_key():
+    key_letters = letters + ["-", "="]
+    string_to_return = ''
+    for x in range(0, 43):
+        string_to_return += key_letters[random.randint(0, 63)]
+    return string_to_return
+
+#begin offuscating recursively
 for i in range(1, passes+1):
-    temp = 'Q'
-    for x in range(0, 20):
-        temp += letters[random.randint(0, 61)]
-    temp_2 = 'Q'
-    for x in range(0, 20):
-        temp_2 += letters[random.randint(0, 61)]
-    temp_3 = 'Q'
-    for x in range(0, 20):
-        temp_3 += letters[random.randint(0, 61)]
-    temp_4 = 'Q'
-    for x in range(0, 20):
-        temp_4 += letters[random.randint(0, 61)]
-    temp_5 = 'Q'
-    for x in range(0, 20):
-        temp_5 += letters[random.randint(0, 61)]
-    temp_6 = 'Q'
-    for x in range(0, 20):
-        temp_6 += letters[random.randint(0, 61)]
-    temp_7 = 'Q'
-    for x in range(0, 20):
-        temp_7 += letters[random.randint(0, 61)]
-    temp_8 = 'Q'
-    for x in range(0, 20):
-        temp_8 += letters[random.randint(0, 61)]
-    temp_9 = 'Q'
-    for x in range(0, 20):
-        temp_9 += letters[random.randint(0, 61)]
+    #generate random variable names
+    function_name = generate_random_string()
+    parameter_name_1 = generate_random_string()
+    parameter_name_2 = generate_random_string()
+    return_variable_name = generate_random_string()
+    charecter_variable_name = generate_random_string()
+    index_of_char_variable = generate_random_string()
+    random_base64_contence = generate_random_string()
+    cipher_text_variable = generate_random_string()
+    decryption_key_variable = generate_random_string()
     temp_list = []
     for x in range(0, 100):
-        temp_10 = 'Q'
-        for y in range(0, 20):
-            temp_10 += letters[random.randint(0, 61)]
-        temp_list.append(temp_10)
-    list_of_vars = deepcopy(temp_list)
-    temp_list.append(temp_9)
+        temp_list.append(generate_random_string())
+
+    #hide key in list
+    temp_list.append(decryption_key_variable)
     random.shuffle(temp_list)
     temp_11 = ''
 
-
+    #introduce deoffuscation functions
     def generate_decrypt_2():
-        return f'''def {temp}({temp_2}, {temp_3}):
+        return f'''def {function_name}({parameter_name_1}, {parameter_name_2}):
     try:
-        {temp_4} = Fernet({temp_2}.encode('utf-8'))
-        return {temp_4}.decrypt({temp_3}.encode('utf-8')).decode('utf-8')
+        {return_variable_name} = Fernet({parameter_name_1}.encode('utf-8'))
+        return {return_variable_name}.decrypt({parameter_name_2}.encode('utf-8')).decode('utf-8')
     except:
         pass
 '''
-
-
     def generate_caesar_function():
-        return f'''def {temp}({temp_2}, {temp_3}):
+        return f'''def {function_name}({parameter_name_1}, {parameter_name_2}):
     try:
-        {temp_4} = ""
-        for {temp_5} in {temp_3}:
-            {temp_6} = ord({temp_5})
-            {temp_4} += chr(({temp_6} - {temp_2}) % 256)
-        return {temp_4}
+        {return_variable_name} = ""
+        for {charecter_variable_name} in {parameter_name_2}:
+            {index_of_char_variable} = ord({charecter_variable_name})
+            {return_variable_name} += chr(({index_of_char_variable} - {parameter_name_1}) % 256)
+        return {return_variable_name}
     except:
         pass
 '''
-
+    
+    #read contence of target file
     try:
         with open(file_to_obfuscate, "r", encoding='utf-8') as file:
             content = file.read()
     except FileNotFoundError:
         print("File not found!")
         exit()
-    if len(content) >= 8869782:
+    if len(content) >= 1169782:
         print("File too big, terminating!")
         exit()
+
+    #choose encryption function at random
     if random.randint(1,2) == 2:
         method = "ceaser"
         rand_key = random.randint(1, 255)
@@ -112,25 +106,28 @@ for i in range(1, passes+1):
         base64_content = base64.b64encode(encrypted_content.encode("latin-1")).decode("ascii")
         func_to_use = generate_decrypt_2()
 
+    #genarate dud keys
     for x in temp_list:
-        if x != temp_9:
-            if random.randint(1,2) == 1:
-                temp_12 = 'Q'
-                for y in range(0, 43):
-                    temp_12 += letters[random.randint(0, 61)]
-                temp_11 += f'{x} = {temp}("{temp_12}", {temp_8})\n'
+        if x != decryption_key_variable:
+            if method == "encryption":
+                temp_12 = generate_random_key()
+                temp_11 += f'{x} = {function_name}("{temp_12}", {cipher_text_variable})\n'
             else:
                 temp_12 = random.randint(1, 255)
-                temp_11 += f'{x} = {temp}({temp_12}, {temp_8})\n'
+                while temp_12 == rand_key:
+                    temp_12 = random.randint(1, 255)
+                temp_11 += f'{x} = {function_name}({temp_12}, {cipher_text_variable})\n'
         else:
-            temp_11 += f'{temp_9} = {temp}({rand_key}, {temp_8})\n'
+            temp_11 += f'{decryption_key_variable} = {function_name}({rand_key}, {cipher_text_variable})\n'
+            
+    #determine final output string
     obfuscated_code = f'''import base64
 from cryptography.fernet import Fernet
-{temp_7} = "{base64_content}"
+{random_base64_contence} = "{base64_content}"
 
 {func_to_use}
 
-{temp_8} = base64.b64decode({temp_7}).decode("latin-1")
+{cipher_text_variable} = base64.b64decode({random_base64_contence}).decode("latin-1")
 {temp_11}
 rand_list = {str(temp_list).replace("'", "")}
 for i in rand_list:
@@ -139,6 +136,7 @@ for i in rand_list:
     except:
         pass
 '''
+    #write to file
     with open(file_to_obfuscate, "w", encoding='utf-8') as new_file:
         new_file.write(obfuscated_code)
     print(f"File obfuscated to: {file_to_obfuscate} on pass {i} using {method}.")
